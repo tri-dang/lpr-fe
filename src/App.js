2ae0116fa106469abedcfd3a18e198b03d513991
A Teo <dangtoantri@gmail.com>
2020-11-28 14:02:05 +0700

    Add routing for categories and category by slug page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Container } from 'react-bootstrap';
+import { Switch, Route } from "react-router-dom";
 
 import Nav from './components/Nav';
 import Categories from "./components/Categories";
+import { RoutedComponent as Category } from "./components/Category";
 
 const containerStyles = {
   marginTop: 10
@@ -12,7 +14,10 @@ function App() {
     <div className="App">
       <Container style={containerStyles}>
         <Nav />
-        <Categories />
+        <Switch>
+          <Route path="/" component={Categories} exact />
+          <Route path="/categories/:slug" component={Category} exact />
+        </Switch>
       </Container>
     </div>
   );
