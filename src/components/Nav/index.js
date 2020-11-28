7fa7c171ddcc0a6bcf07fd67325d4d9b70d1b28c
A Teo <dangtoantri@gmail.com>
2020-11-28 14:05:41 +0700

    Logo by height

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -10,7 +10,7 @@ const Nav = ({ logoMeta }) => (
       return (
         <>
           <Link to='/'>
-            <img src={metaBySlug.picture[0].url} width='50' alt='' />
+            <img src={metaBySlug.picture[0].url} height='50' alt='' />
           </Link>
           <br/>
           <br/>
