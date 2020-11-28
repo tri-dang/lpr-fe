e60de3cabe321c96297f109e80c387b733e4a613
A Teo <dangtoantri@gmail.com>
2020-11-28 13:44:43 +0700

    Move Nav into separated component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { Container } from 'react-bootstrap';
 
-import Query from "./components/Query";
+import Nav from './components/Nav';
 import Categories from "./components/Categories";
-import META_BY_SLUG_QUERY from "./queries/meta/metaBySlug";
 
 const containerStyles = {
   marginTop: 10
@@ -12,18 +11,8 @@ function App() {
   return (
     <div className="App">
       <Container style={containerStyles}>
-        <Query query={META_BY_SLUG_QUERY} slug='logo' spinnerDisabled>
-          {({ data: { metaBySlug } }) => {
-            return (
-              <div>
-                <img src={metaBySlug.picture[0].url} width='50' alt='' />
-                <br/>
-                <br/>
-                <Categories />
-              </div>
-            )
-          }}
-        </Query>
+        <Nav />
+        <Categories />
       </Container>
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,48 +1,24 @@
 import React from "react";
-import Query from "../Query";
 import { Link } from "react-router-dom";
 
-import ALBUMS_QUERY from "../../queries/album/albums";
+import Query from "../Query";
+import META_BY_SLUG_QUERY from "../../queries/meta/metaBySlug";
 
-const Nav = () => {
-  return (
-    <div>
-      <Query query={ALBUMS_QUERY} id={null}>
-        {({ data: { albums } }) => {
-          return (
-            <div>
-              <nav className="uk-navbar-container" data-uk-navbar>
-                <div className="uk-navbar-left">
-                  <ul className="uk-navbar-nav">
-                    <li>
-                      <Link to="/">Strapi Blog</Link>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="uk-navbar-right">
-                  <ul className="uk-navbar-nav">
-                    {albums.map((album, i) => {
-                      return (
-                        <li key={album.id}>
-                          <Link
-                            to={`/album/${album.id}`}
-                            className="uk-link-reset"
-                          >
-                            {album.title} - {album.slug}
-                          </Link>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
-              </nav>
-            </div>
-          );
-        }}
-      </Query>
-    </div>
-  );
-};
+const Nav = ({ logoMeta }) => (
+  <Query query={META_BY_SLUG_QUERY} slug='logo' spinnerDisabled>
+    {({ data: { metaBySlug } }) => {
+      return (
+        <>
+          <Link to='/'>
+            <img src={metaBySlug.picture[0].url} width='50' alt='' />
+          </Link>
+          <br/>
+          <br/>
+        </>
+      )
+    }}
+  </Query>
+);
 
 export default Nav;
+
