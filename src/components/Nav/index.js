ecd14ab03300f0e861ee3ab269efac61241da6b5
A Teo <dangtoantri@gmail.com>
2020-11-30 11:33:33 +0700

    Edit logo width

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,13 +4,18 @@ import { Link } from "react-router-dom";
 import Query from "../Query";
 import META_BY_SLUG_QUERY from "../../queries/meta/metaBySlug";
 
+const logoStyles = {
+  width: '15vw',
+  maxWidth: '100px'
+};
+
 const Nav = ({ logoMeta }) => (
   <Query query={META_BY_SLUG_QUERY} slug='logo' spinnerDisabled>
     {({ data: { metaBySlug } }) => {
       return (
         <>
           <Link to='/'>
-            <img src={metaBySlug.picture[0].url} height='100' alt='' />
+            <img src={metaBySlug.picture[0].url} style={logoStyles} alt={metaBySlug.name} />
           </Link>
           <br/>
           <br/>
