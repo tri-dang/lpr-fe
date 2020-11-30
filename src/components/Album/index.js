46516b4398173e4a24bf463756f81256fe28572c
A Teo <dangtoantri@gmail.com>
2020-11-30 15:11:03 +0700

    Retouch album page css

--- src/components/Album/index.js
+++ src/components/Album/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from 'styled-components';
 import { useParams } from "react-router";
 import { Row, Col, Carousel } from 'react-bootstrap';
 
@@ -6,26 +7,34 @@ import Query from "../Query";
 import ALBUM_BY_SLUG_QUERY from "../../queries/album/albumBySlug";
 import { Photo } from '../shared/Photo';
 
+const StyledRow = styled(Row)`
+  height: calc((100vw - 30px) * 3 / 4);
+  background: black;
+  @media(max-width: 768px) {
+    height: calc((100vw - 30px) * 3 / 2) !important;
+  }
+`;
+
 const Content = ({ album: { galery: { photos }, title, slug, cover }}) => (
   <>
     <Row className="justify-content-md-center">
       <Col md={6}>
         <h3>
-          {title}
+        {title}
         </h3>
       </Col>
     </Row>
     <Row className="justify-content-md-center">
-      <Col sm={12} md={6}>
+      <Col md={6}>
         <Carousel>
           {
             photos.map((photo, i) =>
               <Carousel.Item key={i}>
-                <Row style={{ height: 'calc((100vw - 30px) * 3 / 2)', background: 'black' }}>
-                  <Col xs={12} style={{ margin: 'auto 0' }}>
+                <StyledRow>
+                  <Col style={{ margin: 'auto 0' }}>
                     <Photo picture={photo.picture} title={photo.title}  />
                   </Col>
-                </Row>
+                </StyledRow>
               </Carousel.Item>
             )
           }
