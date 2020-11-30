1ea4ac7c2b42a2f935530bfd20af23dda43a080a
A Teo <dangtoantri@gmail.com>
2020-11-30 15:38:08 +0700

    Disable link to album page

--- src/components/Albums/index.js
+++ src/components/Albums/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "../shared/Link";
 
 import Item from './Item';
 
@@ -7,9 +6,7 @@ const Albums = ({ albums }) => (
   <>
     {albums.map((album, i) =>
       album.galery && album.galery.photos.length > 0 &&
-      <Link to={`/albums/${album.slug}`}>
-        <Item album={album} key={i} />
-      </Link>
+      <Item album={album} key={i} />
     )}
   </>
 );
