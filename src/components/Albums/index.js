3876452e8fa4f99bb358cd4837d582d465a169eb
A Teo <dangtoantri@gmail.com>
2020-11-30 12:12:11 +0700

    retouch css album link

--- src/components/Albums/index.js
+++ src/components/Albums/index.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Item from './Item';
+
+const albumLinkStyle = {
+  color: 'inherit',
+  textDecoration: 'inherit'
+};
 
 const Albums = ({ albums }) => (
   <>
     {albums.map((album, i) =>
-      album.galery && album.galery.photos.length > 0 && <Item album={album} key={i} />
+      album.galery && album.galery.photos.length > 0 &&
+      <Link to={`/albums/${album.slug}`} style={albumLinkStyle}>
+        <Item album={album} key={i} />
+      </Link>
     )}
   </>
 );
