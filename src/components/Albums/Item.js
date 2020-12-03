0b6b503afbf106abecec2b73030b72afb3f3643d
A Teo <dangtoantri@gmail.com>
2020-12-03 23:45:58 +0700

    Change cover to selected image by clicking

--- src/components/Albums/Item.js
+++ src/components/Albums/Item.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col } from 'react-bootstrap';
 
 import Image from '../shared/Image';
@@ -6,44 +6,48 @@ import Thumbnail from '../shared/Thumbnail';
 
 const photoContainerStyles = {
   margin: 'auto 0',
-  paddingBottom: 45
+  paddingBottom: 30,
+  cursor: 'pointer'
 };
 
-const ImageItem = ({ image }) => (
-  <Col md={4} sm={4} xs={4} style={photoContainerStyles}>
+const ImageItem = ({ image, onClick }) => (
+  <Col md={4} sm={4} xs={4} style={photoContainerStyles} onClick={() => onClick(image)}>
     <Thumbnail image={image}  />
   </Col>
 );
 
-const Item = ({ album: { galery: { images }, title, slug, cover }}) => (
-  <>
-    <Row>
-      <Col>
-        <h3>
-          {title}
-        </h3>
-      </Col>
-    </Row>
-    <Row>
-      <Col md={6}>
-        <Image image={cover}  />
-        <br/>
-        <br/>
-      </Col>
-      <Col md={6}>
-        <Row>
-          {
-            images.slice(0, 9).map((image, i) =>
-              <ImageItem image={image} />
-            )
-          }
-        </Row>
-      </Col>
-    </Row>
-    <br/>
-    <br/>
-    <br/>
-  </>
-);
+const Item = ({ album: { galery: { images }, title, slug, cover }}) => {
+  const [selectedImage, setSelectedImage] = useState(0);
+  return (
+    <>
+      <Row>
+        <Col>
+          <h3>
+            {title}
+          </h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6} style={{ margin: 'auto' }}>
+          <Image image={selectedImage || cover}  />
+          <br/>
+          <br/>
+        </Col>
+        <Col md={6}>
+          <Row>
+            {
+              images.slice(0, 9).map((image, i) =>
+                <ImageItem image={image} onClick={setSelectedImage} />
+              )
+            }
+          </Row>
+        </Col>
+      </Row>
+      <br/>
+      <br/>
+      <br/>
+    </>
+  )
+};
 
 export default Item;
