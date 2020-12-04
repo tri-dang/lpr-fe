eb0c34eb21ec56bb0577ceea337bbd97ee7e10af
A Teo <dangtoantri@gmail.com>
2020-12-04 11:16:41 +0700

    Add meta to categories page

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
+import MetaTags from 'react-meta-tags';
 
 import Query from "../Query";
 import Item from './Item';
@@ -8,6 +9,19 @@ import CATEGORIES_QUERY from "../../queries/category/categories";
 const Categories = () => {
   return (
     <div>
+      <MetaTags>
+        <meta property="og:title" content="Baby and family photography" />
+        <meta property="og:type" content="Photography business" />
+        <meta property="og:url" content="https://www.laphotorouge.com/" />
+        <meta property="og:image" content="https://res.cloudinary.com/dlgkecjjd/image/upload/v1606714969/small_DSC_5109_1_d2d1b54d4e.jpg" />
+
+        <meta name="twitter:card" content="https://res.cloudinary.com/dlgkecjjd/image/upload/v1606714969/small_DSC_5109_1_d2d1b54d4e.jpg"/>
+        <meta name="twitter:site" content="La photo Rouge"/>
+        <meta name="twitter:title" content="La photo Rouge"/>
+        <meta name="twitter:description" content="Baby and family photography with on demand or preset concepts"/>
+        <meta name="twitter:creator" content="@author_handle"/>
+        <meta property="twitter:image:src" content="https://res.cloudinary.com/dlgkecjjd/image/upload/v1606714969/small_DSC_5109_1_d2d1b54d4e.jpg" />
+      </MetaTags>
       <Query query={CATEGORIES_QUERY}>
         {({ data: { categories } }) => {
           return (
