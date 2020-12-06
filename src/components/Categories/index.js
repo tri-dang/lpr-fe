3f1ae6c8af5b5f5633614193a514cf36bd557d9b
A Teo <dangtoantri@gmail.com>
2020-12-06 12:08:58 +0700

    Move meta tag to index.html

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -15,11 +15,11 @@ const Categories = () => {
         <meta property="og:url" content="https://www.laphotorouge.com/" />
         <meta property="og:image" content="https://res.cloudinary.com/dlgkecjjd/image/upload/v1606714969/small_DSC_5109_1_d2d1b54d4e.jpg" />
 
-        <meta name="twitter:card" content="https://res.cloudinary.com/dlgkecjjd/image/upload/v1606714969/small_DSC_5109_1_d2d1b54d4e.jpg"/>
-        <meta name="twitter:site" content="La photo Rouge"/>
-        <meta name="twitter:title" content="La photo Rouge"/>
-        <meta name="twitter:description" content="Baby and family photography with on demand or preset concepts"/>
-        <meta name="twitter:creator" content="La photo Rouge"/>
+        <meta property="twitter:card" content="https://res.cloudinary.com/dlgkecjjd/image/upload/v1606714969/small_DSC_5109_1_d2d1b54d4e.jpg"/>
+        <meta property="twitter:site" content="La photo Rouge"/>
+        <meta property="twitter:title" content="La photo Rouge"/>
+        <meta property="twitter:description" content="Baby and family photography with on demand or preset concepts"/>
+        <meta property="twitter:creator" content="La photo Rouge"/>
         <meta property="twitter:image:src" content="https://res.cloudinary.com/dlgkecjjd/image/upload/v1606714969/small_DSC_5109_1_d2d1b54d4e.jpg" />
       </MetaTags>
       <Query query={CATEGORIES_QUERY}>
