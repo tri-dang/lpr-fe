fbc18afd57bcfd9bcac2190ab81c0b81f58b7afe
A Teo <dangtoantri@gmail.com>
2020-12-06 12:23:39 +0700

    Remove react-helmet

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
-import {Helmet} from "react-helmet";
 
 import Query from "../Query";
 import Item from './Item';
@@ -9,17 +8,6 @@ import CATEGORIES_QUERY from "../../queries/category/categories";
 const Categories = () => {
   return (
     <div>
-      <Helmet>
-        <meta property="og:title" content="Baby and family photography" />
-        <meta property="og:type" content="Categories" />
-        <meta property="og:url" content="https://www.laphotorouge.com/" />
-        <meta property="og:image" content="https://res.cloudinary.com/dlgkecjjd/image/upload/v1606714969/small_DSC_5109_1_d2d1b54d4e.jpg" />
-
-        <meta name="twitter:card" content="summary"/>
-        <meta name="twitter:site" content="La photo Rouge"/>
-        <meta name="twitter:creator" content="La photo Rouge"/>
-        <meta name="twitter:description" content="Baby and family photography with on demand or preset concepts"/>
-      </Helmet>
       <Query query={CATEGORIES_QUERY}>
         {({ data: { categories } }) => {
           return (
--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useParams } from "react-router";
-import MetaTags from 'react-meta-tags';
 
 import Query from "../Query";
 import Albums from '../Albums';
@@ -10,19 +9,6 @@ const Category = ({ slug }) => (
   <Query query={CATEGORY_BY_SLUG_QUERY} slug={slug}>
     {({ data: { categoryBySlug: { albums, name, cover } } }) =>
     <>
-      <MetaTags>
-        <meta property="og:title" content={name} />
-        <meta property="og:type" content="Category" />
-        <meta property="og:url" content={`https://www.laphotorouge.com/categories/${slug}`} />
-        <meta property="og:image" content={cover.formats.thumbnail.url} />
-
-        <meta name="twitter:card" content={cover.formats.thumbnail.url}/>
-        <meta name="twitter:site" content="La photo Rouge"/>
-        <meta name="twitter:title" content={name}/>
-        <meta name="twitter:description" content={name}/>
-        <meta name="twitter:creator" content="La photo Rouge"/>
-        <meta property="twitter:image:src" content={cover.formats.thumbnail.url} />
-      </MetaTags>
       <Albums albums={albums} />
     </>
     }
