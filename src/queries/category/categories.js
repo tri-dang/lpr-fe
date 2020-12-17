03d2589a9c3f5c4db052d29e6ce0ad2bda27ea5b
A Teo <dangtoantri@gmail.com>
2020-12-17 13:29:49 +0700

    Sort categories query by position asc

--- src/queries/category/categories.js
+++ src/queries/category/categories.js
@@ -2,10 +2,11 @@ import gql from "graphql-tag";
 
 const CATEGORIES_QUERY = gql`
   query Categories {
-    categories {
+    categories (sort: "position:asc") {
       id
       name
       slug
+      position
       cover {
         formats
       }
